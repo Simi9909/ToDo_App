fc486ec585328921b61cf51d479afa97c18e3b42
Simion Tamás <tamas.simon12@gmail.com>
2021-05-15 19:28:25 +0200

    Adding Insert, Update Delete method

--- src/java/controller/MainController.java
+++ src/java/controller/MainController.java
@@ -1,16 +1,26 @@
 package controller;
 
 import java.net.URL;
+import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
+
+import database.DBConnection;
+import database.DateBase;
+import database.TaskRepository;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import model.Task;
+import model.TransferUtility;
+
+import javax.transaction.Transactional;
+
+import static java.lang.Integer.parseInt;
 
 public class MainController {
 
@@ -59,29 +69,116 @@ public class MainController {
         @FXML
         private Button btn_exit;
 
-        @FXML
-        void Delete(ActionEvent event) {
+        private Task task;
 
+        public Button getBtn_refresh() {
+                return btn_refresh;
+        }
+
+        public void setBtn_refresh(Button btn_refresh) {
+                this.btn_refresh = btn_refresh;
         }
 
         @FXML
-        void Exit(ActionEvent event) {
+        private Button btn_refresh;
+
+        private TaskRepository createNewTask = new TaskRepository();
+
+        @FXML
+        void initialize() {
+                DBConnection.openEmf();
+                Refresh();
+                tv_tasks.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
+                        if (newVal != null) {
+                                tf_task.setText(newVal.getTask());
+                                tf_person.setText(newVal.getPerson());
+                                dp_startdate.setValue(newVal.getStartdate());
+                                dp_finishdate.setValue(newVal.getFinishdate());
+                        }
+                });
+        }
+
+        private  void initColumn(){
+                col_task.setCellValueFactory(new PropertyValueFactory<Task, String>("task"));
+                col_person.setCellValueFactory(new PropertyValueFactory<Task, String>("person"));
+                col_startdate.setCellValueFactory(new PropertyValueFactory<Task, LocalDate>("startdate"));
+                col_finishdate.setCellValueFactory(new PropertyValueFactory<Task, LocalDate>("finishdate"));
 
         }
 
         @FXML
         void Insert(ActionEvent event) {
+                try {
+                        Task newTask = new Task();
 
+                        newTask.setTask(tf_task.getText());
+                        newTask.setPerson(tf_person.getText());
+                        newTask.setStartdate(dp_startdate.getValue());
+                        newTask.setFinishdate(dp_finishdate.getValue());
+
+                        createNewTask.AddNewTask(newTask);
+
+                } catch ( Exception e) {
+                        System.out.println("Invalid data detected");
+                }
+                Refresh();
         }
 
         @FXML
         void Update(ActionEvent event) {
+                TransferUtility.task = tv_tasks.getSelectionModel().getSelectedItem();
+
+                try {
+                        Task newTask = new Task();
+
+                        newTask.setTask(tf_task.getText());
+                        newTask.setPerson(tf_person.getText());
+                        newTask.setStartdate(dp_startdate.getValue());
+                        newTask.setFinishdate(dp_finishdate.getValue());
+
+                        createNewTask.UpdateTask(newTask);
+
+                        System.out.println("Update successfully");
+                } catch (Exception e){
+                        System.out.println("Something went wrong in update");
+                }
+
+                Refresh();
 
         }
 
         @FXML
-        void initialize() {
+        @Transactional
+        public void Delete(ActionEvent event) {
+                createNewTask.DeleteTask(TransferUtility.task = tv_tasks.getSelectionModel().getSelectedItem());
+                Refresh();
 
         }
-    }
+
+        @FXML
+        void Exit(ActionEvent event) {
+                Platform.exit();
+                System.exit(0);
+        }
+
+        @FXML
+        void Refresh(){
+                try {
+                        ObservableList<Task> data = FXCollections.observableArrayList(
+                                createNewTask.Query());
+                        tv_tasks.setItems(data);
+                        initColumn();
+
+                }catch (Exception e){
+                        System.out.println("Something went wrong in table refresh");
+                }
+
+
+                tf_task.clear();
+                tf_person.clear();
+                dp_startdate.setValue(null);
+                dp_finishdate.setValue(null);
+        }
+
+}
 
