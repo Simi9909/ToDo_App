08c8358b5e5087d8db0cdfff9d2ae0c4b3834a59
Simion Tamás <tamas.simon12@gmail.com>
2021-05-18 01:24:33 +0200

    Minor bugfix in isNotEmpty() method

--- src/test/java/CheckIsEmptyTest.java
+++ src/test/java/CheckIsEmptyTest.java
@@ -22,7 +22,7 @@ public class CheckIsEmptyTest {
 
         Check check = new Check();
 
-        boolean isNotEmpty = check.checkIfEmpty("test", "test", LocalDate.now(), LocalDate.now());
+        boolean isNotEmpty = check.checkIfEmpty("test", "test",LocalDate.parse("2021-11-27"), LocalDate.parse("2020-11-27"));
 
         assertFalse(isNotEmpty);
     }
