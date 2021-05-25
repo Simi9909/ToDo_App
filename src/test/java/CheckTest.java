0eacb9259dbf8b8cbfdf85fa583c626b238c54c2
Simion Tamás <tamas.simon12@gmail.com>
2021-05-25 15:30:51 +0200

    Adding tests

--- src/test/java/CheckTest.java
+++ src/test/java/CheckTest.java
@@ -28,11 +28,53 @@ public class CheckTest {
     }
 
     @Test
+    public void isNotEmpty2() {
+
+        Check check = new Check();
+
+        boolean isNotEmpty = check.checkIfEmpty("test2", "test2",LocalDate.parse("2021-11-28"), LocalDate.parse("2020-11-28"));
+
+        assertFalse(isNotEmpty);
+    }
+
+    @Test
+    public void isNotEmpty3() {
+
+        Check check = new Check();
+
+        boolean isNotEmpty = check.checkIfEmpty("test3", "test3",LocalDate.parse("2021-10-10"), LocalDate.parse("2020-11-11"));
+
+        assertFalse(isNotEmpty);
+    }
+
+    @Test
     public void dateIsOk() {
 
         Check check = new Check();
 
-        boolean isCorrect = check.checkDates(LocalDate.parse("2020-11-27"), LocalDate.parse("2021-11-27"));
+        boolean isCorrect = check.checkDates(LocalDate.parse("2020-11-26"), LocalDate.parse("2021-11-27"));
+
+        assertFalse(isCorrect);
+
+    }
+
+    @Test
+    public void dateIsOk2() {
+
+        Check check = new Check();
+
+        boolean isCorrect = check.checkDates(LocalDate.parse("2020-12-27"), LocalDate.parse("2021-12-28"));
+
+        assertFalse(isCorrect);
+
+    }
+
+    @Test
+    public void dateIsOk3() {
+
+        Check check = new Check();
+
+        boolean isCorrect = check.checkDates(LocalDate.parse("2020-11-27"), LocalDate.parse("2022-12-20"));
 
         assertFalse(isCorrect);
 
@@ -50,6 +92,28 @@ public class CheckTest {
     }
 
     @Test
+    public void dateIsWrong2() {
+
+        Check check = new Check();
+
+        boolean isWrong = check.checkDates(LocalDate.parse("2021-11-28"), LocalDate.parse("2020-11-27"));
+
+        assertTrue(isWrong);
+
+    }
+
+    @Test
+    public void dateIsWrong3() {
+
+        Check check = new Check();
+
+        boolean isWrong = check.checkDates(LocalDate.parse("2022-11-28"), LocalDate.parse("2020-10-02"));
+
+        assertTrue(isWrong);
+
+    }
+
+    @Test
     public void isBusy() {
 
         Check check = new Check();
@@ -60,11 +124,31 @@ public class CheckTest {
     }
 
     @Test
+    public void isBusy2() {
+
+        Check check = new Check();
+
+        boolean isBusy = check.checkIfBusy("Tamas", LocalDate.parse("2021-05-18"), LocalDate.parse("2021-05-18"));
+
+        assertTrue(isBusy);
+    }
+
+    @Test
     public void isNotBusy() {
 
         Check check = new Check();
 
         boolean isBusy = check.checkIfBusy("Tamas", LocalDate.parse("2020-11-27"), LocalDate.parse("2020-11-28"));
+
+        assertFalse(isBusy);
+    }
+
+    @Test
+    public void isNotBusy2() {
+
+        Check check = new Check();
+
+        boolean isBusy = check.checkIfBusy("Tamas", LocalDate.parse("2020-12-27"), LocalDate.parse("2020-12-28"));
 
         assertFalse(isBusy);
     }
